56b84a962da9e300aebdc0356f1bae7b90102e49
Thanhdatms <dangthanhdatuit@gmail.com>
2025-04-13 19:42:36 +0700

    feat: estimate deliver time & routes steps

--- PackRunner/settings.py
+++ PackRunner/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'users',
-    'orders'
+    'orders',
+    'location',
 ]
 
 MIDDLEWARE = [
