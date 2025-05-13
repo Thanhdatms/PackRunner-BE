f9c14897dfe4df5c73543e809a8ce1e67e4b990f
Thanhdatms <dangthanhdatuit@gmail.com>
2025-05-13 23:18:39 +0700

    fix: hard code opt to 1111

--- users/views.py
+++ users/views.py
@@ -17,23 +17,30 @@ from api_doc.schemas.auth_schemas import login_schema, register_schema, verify_o
 from utils.response import success_response, fail_response
 from rest_framework.permissions import AllowAny
 from api_doc.schemas.user_schemas import user_profile_schema, address_schema, address_detail_schema
+from rest_framework import serializers
+from utils.response import fail_response, success_response
 # Register API
 
 @extend_schema(tags=['Auth'])
 @register_schema
 class RegisterView(APIView):
     def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        group_name = request.data.get("group", "User")  
-        group, created = Group.objects.get_or_create(name=group_name)
-        user.groups.add(group)
+        try:
+            serializer = UserSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            user = serializer.save()
+            group_name = request.data.get("group", "User")  
+            group, created = Group.objects.get_or_create(name=group_name)
+            user.groups.add(group)
 
-        return Response({
-            "message": "User registered successfully!", 
-            "data": serializer.data
-        })
+            # return Response({
+            #     "message": "User registered successfully!", 
+            #     "data": serializer.data
+            # })
+            return success_response(message="User registered successfully!", data=serializer.data)
+        
+        except Exception as e:
+            return fail_response(str(e), status_code=400)
 
 # Login API
 @extend_schema(tags=['Auth'])
@@ -41,31 +48,34 @@ class RegisterView(APIView):
 class LoginView(APIView):
     permission_classes = [AllowAny]
     def post(self, request):
-        phone_number = request.data.get("phone_number")
-        password = request.data.get("password")
+        try:
+            phone_number = request.data.get("phone_number")
+            password = request.data.get("password")
 
-        if not phone_number or not password:
-            raise fail_response("Phone number and password are required!")
+            if not phone_number or not password:
+                return fail_response("Phone number and password are required!")
 
-        user = User.objects.filter(phone_number=phone_number).first()
-        if user is None:
-            raise fail_response("User not found!")
-        
-        if not user.check_password(password):
-            raise fail_response("Incorrect password!")
-        # Generate OTP
-        user.generate_otp()
-        phone_number = "84" + user.phone_number[1:]
-        otp = user.otp
+            user = User.objects.filter(phone_number=phone_number).first()
+            if user is None:
+                return fail_response("User not found!")
+            
+            if not user.check_password(password):
+                return fail_response("Incorrect password!")
+            # Generate OTP
+            user.generate_otp()
+            phone_number = "84" + user.phone_number[1:]
+            otp = user.otp
 
-        # try:
-        #     sendSmSOTP(phone_number=phone_number, otp=otp)
-        # except Exception as e:
-        #     return fail_response("Failed to send OTP. Please try again later.", status_code=500)
+            # try:
+            #     sendSmSOTP(phone_number=phone_number, otp=otp)
+            # except Exception as e:
+            #     return fail_response("Failed to send OTP. Please try again later.", status_code=500)
 
-        return success_response(message="OTP sent. Please verify to complete login.", data={
-            "phone_number": phone_number, 
-        })
+            return success_response(message="OTP sent. Please verify to complete login.", data={
+                "phone_number": phone_number, 
+            })
+        except Exception as e:
+            return fail_response(str(e), status_code=400)
 
 @extend_schema(tags=['Auth'])
 @verify_opt_schema
@@ -77,13 +87,13 @@ class VerifyOTPView(APIView):
         try:
             user = User.objects.get(phone_number=phone_number)
         except User.DoesNotExist:
-            return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
-        
+            return fail_response("User not found", status_code=404)
+
         if user.otp_max_out and now() < user.otp_max_out:
-            return Response({"error": "Too many failed attempts. Try again later."}, status=status.HTTP_403_FORBIDDEN)
-        
+            return fail_response("Account is locked for 1 hour after max attempts.", status_code=403)
+
         if now() > user.otp_expiry:
-            return Response({"error": "OTP has expired"}, status=status.HTTP_400_BAD_REQUEST)
+            return fail_response("OTP has expired. Please request a new one.", status_code=400)
 
         # Check OTP validity
         if user.otp != otp:
@@ -93,8 +103,8 @@ class VerifyOTPView(APIView):
                 # Lock user for 1 hour if max attempts are exceeded
                 user.otp_max_out = now() + timedelta(hours=1)
             user.save()
-            return Response({"error": "Incorrect OTP. Account is locked for 1 hour after max attempts."}, status=status.HTTP_400_BAD_REQUEST)
-        
+            return fail_response("Incorrect OTP. Please try again.", status_code=400)
+
         if user.otp == otp:
             user.is_active = True
             user.otp = None
@@ -168,10 +178,12 @@ class LogoutView(APIView):
 )    
 class UserListView(APIView):
     def get(self, request):
-        users = User.objects.all()
-        serializer = UserSerializer(users, many = True)
-        return Response({"message": "Successfully", "data": serializer.data })
-
+        try:
+            users = User.objects.all()
+            serializer = UserSerializer(users, many = True)
+            return Response({"message": "Successfully", "data": serializer.data })
+        except Exception as e:
+            return fail_response(str(e), status_code=400)
 @extend_schema(
     request=UserSerializer,
     responses=UserSerializer,
@@ -179,9 +191,12 @@ class UserListView(APIView):
 )       
 class UserDetailView(APIView):
     def get(self, request, id):
-        user = get_object_or_404(User, id=id)
-        serializer = UserSerializer(user)
-        return Response({"message": "Successfully", "data": serializer.data}, status=status.HTTP_200_OK)
+        try:
+            user = get_object_or_404(User, id=id)
+            serializer = UserSerializer(user)
+            return Response({"message": "Successfully", "data": serializer.data}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return fail_response(str(e), status_code=400)
 
 @extend_schema(tags=['User']) 
 @user_profile_schema  
@@ -189,21 +204,27 @@ class UserProfileView(APIView):
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
 
     def get(self, request):
-        user = request.user
-        serializer = UserSerializer(user)
-        return Response({"message": "Successfully", "data": serializer.data})
-    
+        try:
+            user = request.user
+            serializer = UserSerializer(user)
+            return Response({"message": "Successfully", "data": serializer.data})
+        except Exception as e:
+            return fail_response(str(e), status_code=400)
+        
     def put(self, request):
-        user = request.user
-        serializer = UserSerializer(user, data = request.data, partial=True)
+        try:
+            user = request.user
+            serializer = UserSerializer(user, data = request.data, partial=True)
 
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()
+            serializer.is_valid(raise_exception=True)
+            user = serializer.save()
 
-        return Response({
-            "message": "Successfully",
-            "data": serializer.data
-        })
+            return Response({
+                "message": "Successfully",
+                "data": serializer.data
+            })
+        except Exception as e:
+            return fail_response(str(e), status_code=400)
 
 @extend_schema(tags=['Address'])
 @address_schema
@@ -211,26 +232,31 @@ class AddressView(APIView):
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
 
     def post(self, request):
-        serializer = AddressSerializer(data = request.data, context = {'request': request})
+        try:
+            serializer = AddressSerializer(data = request.data, context = {'request': request})
 
-        if serializer.is_valid(raise_exception=True):
-            serializer.save(user = request.user)
-            return Response({
-                "message": "Successfully",
-                "data": serializer.data
-            })
+            if serializer.is_valid(raise_exception=True):
+                serializer.save(user = request.user)
+                return Response({
+                    "message": "Successfully",
+                    "data": serializer.data
+                })
+        except Exception as e:
+            return fail_response(str(e), status_code=400)
 
     def get(self, request):
-        user = request.user
+        try:
+            user = request.user
 
-        addresses = Address.objects.filter(user = user)
-        serializer = AddressSerializer(addresses, many = True)
+            addresses = Address.objects.filter(user = user)
+            serializer = AddressSerializer(addresses, many = True)
 
-        return Response({
-                "message": "Successfully",
-                "data": serializer.data
-            })
-    
+            return Response({
+                    "message": "Successfully",
+                    "data": serializer.data
+                })
+        except Exception as e:
+            return fail_response(str(e), status_code=400)
     
 @extend_schema(tags=['Address'])
 @address_detail_schema
@@ -238,24 +264,30 @@ class AddressDetailView(APIView):
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
 
     def delete(self, request, pk):
-        user = request.user 
-        address = Address.objects.filter(user=user, id=pk).first()
-        
-        if address:
-            address.delete()
-            return Response({"message": "Delete Successfully"}, status=status.HTTP_204_NO_CONTENT)
-        else:
-            return Response({"error": "Address not found"}, status=status.HTTP_404_NOT_FOUND)
-        
+        try:
+            user = request.user 
+            address = Address.objects.filter(user=user, id=pk).first()
+            
+            if address:
+                address.delete()
+                return Response({"message": "Delete Successfully"}, status=status.HTTP_204_NO_CONTENT)
+            else:
+                return Response({"error": "Address not found"}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return fail_response(str(e), status_code=400)
+            
     def patch(self, request, pk):
-        user = request.user
-        address = Address.objects.filter(user=user, id=pk).first()
+        try:
+            user = request.user
+            address = Address.objects.filter(user=user, id=pk).first()
 
-        if not address:
-            return fail_response("Address not found", status_code=404)
+            if not address:
+                return fail_response("Address not found", status_code=404)
 
-        serializer = AddressSerializer(address, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+            serializer = AddressSerializer(address, data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
 
-        return success_response(message="Address updated successfully", data=serializer.data)
+            return success_response(message="Address updated successfully", data=serializer.data)
+        except Exception as e:
+            return fail_response(str(e), status_code=400)
