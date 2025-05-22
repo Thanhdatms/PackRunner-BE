bbfb025ad53c988146eb2e5fbd0b553097420def
Thanhdatms <dangthanhdatuit@gmail.com>
2025-05-22 19:27:03 +0700

    update: dev env

--- PackRunner/settings.py
+++ PackRunner/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'NAME': 'packrunner-db',
         'USER': 'postgres',
         'PASSWORD': 'root',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '5432'
     }
 }
